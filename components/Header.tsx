a475d723db4fcd1f31750fcc56cb9213abfb6bbf
paudefclasspy <pauaranega20@gmail.com>
2025-02-13 15:57:22 +0100

    nombre nuevo header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-lg py-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">Aplicación de Tareas</h1>
+        <h1 className="text-2xl font-bold text-white">ToDo App</h1>
       </div>
     </header>
   )
